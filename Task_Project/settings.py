13e0333fd769af7b4e26ec86751a2666571e732e
NawazAli01 <63184410+NawazAli01@users.noreply.github.com>
2022-01-14 23:53:07 +0530

    commit updated

--- Task_Project/settings.py
+++ Task_Project/settings.py
@@ -21,7 +21,8 @@ TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-apa@nj4%l=@qwj4e63v!&94md5v$c^3j-=2#6)(0b3%8n#(v2a'
+from decouple import config
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
